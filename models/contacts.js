80efff1a6cb4f528e502093e952d37080fc72ee4
Maxim Kucheriaviy <maxim-k@i.ua>
2022-12-06 22:33:58 +0200

    ready

--- models/contacts.js
+++ models/contacts.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises')
 const path = require("path");
+const {v4} = require("uuid");
 
 
 const contactsPath = path.resolve(__dirname + "/contacts.json");
@@ -24,11 +25,55 @@ const getContactById = async (contactId) => {
   }
 }
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  try{
+    const data = await listContacts();
+    const index = data.findIndex(item => item.id === contactId);
+    if(index === -1){
+      return null;
+    }
+    const item = data.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(data));
+    return item;
+  }
+  catch(err){
+    console.log(err);
+  }
+}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  try{
+    const data = await listContacts();
+    body.id = v4();
+    if(data.some(item => item.name === body.name)){
+      return null;
+    }
+    data.push(body);
+    await fs.writeFile(contactsPath, JSON.stringify(data));
+    return body;
+  }
+  catch(err){
+    console.log(err);
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  try{
+    const data = await listContacts();
+    const index = data.findIndex(item => item.id === contactId);
+    if(index === -1){
+      return null;
+    }
+    data[index].name = body.name;
+    data[index].email = body.email;
+    data[index].phone = body.phone;
+    await fs.writeFile(contactsPath, JSON.stringify(data));
+    return data[index];
+  }
+  catch(err){
+    console.log(err);
+  }
+}
 
 module.exports = {
   listContacts,
